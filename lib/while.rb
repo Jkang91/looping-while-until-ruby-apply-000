48773e9003ffc51dfa5a04b8ee4b78f9e2b8b28d
June Young Kang <jkang34@gmail.com>
2020-09-23 19:02:00 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,8 +1,8 @@
 def using_while
   levitation_force = 6
-	while levitation force < 11 do
+	while levitation_force < 11 do
 	  puts "levitation force"
-	  levitation force += 1
+	  levitation_force += 1
 	end 
 end
 
