26ea872768c2c2cd9fba2d2a57a0b7aa8b10dd9f
June Young Kang <jkang34@gmail.com>
2020-09-23 19:04:11 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,8 +1,8 @@
 def using_until
   levitation_force = 6
-  until levitation force = 11 do
+  until levitation_force = 11 do
     puts "Wingardium Leviosa"
-    levitation force += 1
+    levitation_force += 1
   end
 end
 
